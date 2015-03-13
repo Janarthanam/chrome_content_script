8a7f5f9678d579da45eb46f7fa9d19965bec9456
janar <janar@yahoo-inc.com>
2015-03-13 06:53:50 -0700

    introduce content script to load on login fill in the blanks page

--- chrome_content_script/initiate_code.js
+++ chrome_content_script/initiate_code.js
@@ -3,9 +3,17 @@ var blueToothExtensionId = "llgolpdebgflaknadolcbjblnljfnngd";
 
 console.log("I am in the content script");
 
-chrome.runtime.sendMessage(blueToothExtensionId,{"type": "code"}, function(response) {
-    if (!response)
-      console.log("last error: " + chrome.runtime.lastError.message);
-    else
-      console.log("Response from app: " + response.code);
-});
+
+e = document.getElementById("login-otp-passwd");
+
+if(e != null){
+  console.log(e.value);
+  chrome.runtime.sendMessage(blueToothExtensionId,{"type": "code"}, function(response) {
+      if (!response)
+        console.log("last error: " + chrome.runtime.lastError.message);
+      else{
+        console.log("Response from app: " + response.code);
+        e.value = response.code;
+      }
+  });
+}
